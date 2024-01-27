f99b1d118117c6ba2f104fe2addd3987cc8be084
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-01-27 23:01:09 +0530

    Format code and removed unused files

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname
-    const publicPath = path === '/login' || path === '/register'
+  const path = request.nextUrl.pathname
+  const publicPath = path === '/login' || path === '/register'
   const token = request.cookies.get('token')?.value || ""
 
-    if(publicPath && token) {
-        return NextResponse.redirect(new URL('/', request.url))
-    }
+  if (publicPath && token) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
 
-    if(!publicPath && !token) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+  if (!publicPath && !token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
