11824f2e26bc90d27c70e87b9956320df02c7ca6
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-01-29 22:37:52 +0530

    FIxed- tailwind issue

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,27 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname
-  const publicPath = path === '/login' || path === '/register'
-  const token = request.cookies.get('token')?.value || ""
+    if (request.nextUrl.pathname.startsWith("/_next")) {
+        return NextResponse.next();
+    }
+    const path = request.nextUrl.pathname
+    const publicPath = path === '/login' || path === '/register'
+    const token = request.cookies.get('token')?.value || ""
 
-  if (publicPath && token) {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+    if (publicPath && token) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
 
-  if (!publicPath && !token) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+    if (!publicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    '/:path*',
-    '/login',
-    '/register'
-  ]
+    matcher: [
+        // '/:path*',
+        '/login',
+        '/register',
+    ]
 }
