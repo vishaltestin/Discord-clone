7ff6d307043f7116e029821a41c93ed50c233060
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-02 09:48:22 +0530

    Test - Server

---
+++ helpers/getDataFromtoken.ts
@@ -0,0 +1,11 @@
+// import { NextRequest } from "next/server";
+// import jwt from "jsonwebtoken"
+// export const getDataFromToken = () => {
+//     try {
+//         const encodedtoken = req.cookies.get('token')?.value || ''
+//         const decodedToken: any = jwt.verify(encodedtoken, process.env.JWT_SECRET || "")
+//         return decodedToken.userId
+//     } catch (error: any) {
+//         throw new Error(error.message)
+//     }
+// }
