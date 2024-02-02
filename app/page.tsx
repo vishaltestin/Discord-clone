172c3ddebe372808d3dfac39858714e715df8571
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-02 22:58:35 +0530

    Fixed - Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,66 @@
-import { ModeToggle } from "@/components/mode-toggle";
+"use client"
 import { redirect } from "next/navigation";
-import { db } from "@/lib/db";
-import { initialProfile } from "@/lib/initial-profile";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
-const Home = async () => {
-  const userString = localStorage.getItem('authData');
-  if (!userString) {
-    redirect('/login');
-  }
-  const user = JSON.parse(userString);
-  const profile = await initialProfile(user.id);
-  const server = await db.server.findFirst({
-    where: {
-      members: {
-        some: {
-          profileId: profile.id
-        }
-      }
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    console.log('test')
+    const userString = localStorage.getItem('authData');
+
+    if (!userString) {
+      redirect('/login');
     }
-  });
 
-  if (server) {
-    return redirect(`/servers/${server.id}`);
+    const user = JSON.parse(userString);
+
+    if (!user || !user.userId) {
+      console.error('User data is missing userId property');
+      redirect('/login');
+    }
+
+    const fetchData = async () => {
+      console.group(user.userId)
+      try {
+        const response = await axios.post('/api/userinfo', user);
+        const data = response.data;
+
+        if (data.success) {
+          setProfile(data.data);
+          console.log(data.data)
+        } else {
+          redirect('/login');
+        }
+      } catch (error) {
+        console.error(error);
+        redirect('/login');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  ;
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
+  if (!profile) {
+    return <div>Error fetching profile data</div>;
+  }
+
+  // Rest of your logic here using the 'profile' variable
+
   return (
     <div>
       create a server
     </div>
   );
-
 }
 
 export default Home;
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <h1>Hello Discord</h1>
-//       <ModeToggle />
-//     </div>
-//   );
-// }
