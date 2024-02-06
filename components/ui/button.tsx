28f96162360e5c889fee2e933fa1ad7ea46bbbd2
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-06 09:45:26 +0530

    Modified - custom Varient

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        primary: "bg-indigo-500 text-white hover:bg-indigo-500/90",
+        discord: "rounded-md border border-solid border-background-modifier-accent bg-background-primary mb-8 flex items-center cursor-pointer w-full p-0"
       },
       size: {
         default: "h-10 px-4 py-2",
