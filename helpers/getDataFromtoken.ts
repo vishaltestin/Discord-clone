ad73a51634f2a7d93a37d38b7b33d9e946c8f6e1
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-06 09:45:00 +0530

    Added - Helper Function

--- helpers/getDataFromtoken.ts
+++ helpers/getDataFromtoken.ts
@@ -1,11 +1,14 @@
-// import { NextRequest } from "next/server";
-// import jwt from "jsonwebtoken"
-// export const getDataFromToken = () => {
-//     try {
-//         const encodedtoken = req.cookies.get('token')?.value || ''
-//         const decodedToken: any = jwt.verify(encodedtoken, process.env.JWT_SECRET || "")
-//         return decodedToken.userId
-//     } catch (error: any) {
-//         throw new Error(error.message)
-//     }
-// }
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken"
+export const getDataFromToken = (req: NextRequest) => {
+    try {
+        const encodedtoken = req.cookies.get('token')?.value || ''
+        console.log(encodedtoken,"test")
+        const decodedToken: any = jwt.verify(encodedtoken, process.env.JWT_SECRET || "")
+        console.log(decodedToken)
+        return decodedToken.userId
+    } catch (error: any) {
+        console.log(error,"error")
+        throw new Error(error.message)
+    }
+}
