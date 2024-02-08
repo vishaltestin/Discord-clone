5ba5cd1df21009f13ca0b20f596ca28842d5d083
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-08 22:08:43 +0530

    Added - Curret  Logged in User Profile

---
+++ lib/Profile.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import { getDataFromToken } from "@/helpers/getDataFromtoken";
+import { db } from "./db";
+
+export const presentProfile = async (req: NextRequest) => {
+    try {
+        const userId = getDataFromToken(req);
+        console.log(userId,"test hreeeeeeeeeeeee")
+        if (!userId) {
+            return null
+        }
+        const profile = await db.profile.findUnique({
+            where: {
+                userId
+            }
+        })
+
+        return profile;
+    } catch (error) {
+        console.log(error,'error here')
+        throw new Error("Authentication failed");
+    }
+};
+
