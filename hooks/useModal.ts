c3073044005e5c7fc44a96f1be65f177a89c8bc1
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-14 10:13:09 +0530

    Modifed - Use Modal Hook

--- hooks/useModal.ts
+++ hooks/useModal.ts
@@ -1,9 +1,11 @@
-import { Server } from "@prisma/client";
+import { ChannelType, Server,Channel } from "@prisma/client";
 import { create } from "zustand"
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer"
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel"
 interface ModalData {
-    server?: Server
+    server?: Server,
+    channel?: Channel,
+    channelType?: ChannelType
 }
 interface ModalStore {
     type: ModalType | null;
