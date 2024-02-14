e2794fe5ad1d5a6b916df0a2ec5279ec424a896e
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-02-14 10:13:46 +0530

    Modifed - Redirect user to Default General Channel

--- app/(Start)/servers/[serverId]/page.tsx
+++ app/(Start)/servers/[serverId]/page.tsx
@@ -1,9 +1,50 @@
-const ServerPage = () => {
-    return (
-        <div>
-            Server Page
-        </div>
-    );
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { presentProfile } from "@/lib/Profile";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+};
+
+const ServerIdPage = async ({
+    params
+}: ServerIdPageProps) => {
+    const profile = await presentProfile();
+
+    if (!profile) {
+        return redirect('/login')
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    })
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 
-export default ServerPage;
+export default ServerIdPage;
